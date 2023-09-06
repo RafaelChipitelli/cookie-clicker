ea387bde0bfc7018cf9fb033431f8bbfd396dda2
Rafael Chipitelli <RafaelChipitelli.o.c27@gmail.com>
2023-09-05 22:49:33 -0300

    cookie ao clicar em dobro

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let cookies = 0;
+let clickMultiplier = 1;
+let clickCount = 0; // Contagem de cliques
+let bonusActive = false; // Variável para rastrear se o bônus está ativo
+let bonusTimeout; // Variável para rastrear o tempo limite do bônus
+let clickGoal = 10; // Variável para setar o bonus duplo
+const timeForBonusOff = 1000; // Constante para o tempo até acabar o bonus em milessegundos
 
 const cookie = document.getElementById('cookie');
 const cookiesCount = document.getElementById('cookiesCount');
 const buyAutoClickButton = document.getElementById('buyAutoClick');
+const bonusMessage = document.getElementById('bonusMessage'); // Parágrafo de bônus
 let autoClicks = 0;
 let autoClickPrice = 100;
 
@@ -29,33 +36,65 @@ buyAutoClickButton.addEventListener('click', () => {
     }
 });
 
+function applyClickMultiplier() {
+    clickMultiplier = 2;
+    bonusMessage.style.display = 'block'; // Exibe a mensagem de bônus
+
+    // Define o temporizador do bônus para desativar o bônus após 1 segundo
+    bonusTimeout = setTimeout(() => {
+        clickMultiplier = 1;
+        bonusMessage.style.display = 'none'; // Oculta a mensagem de bônus
+        clickCount = 0; // Reinicia a contagem após 1 segundo
+        bonusActive = false; // Bônus desativado
+    }, timeForBonusOff); // segundos em milissegundos
+}
+
 function updateCookiesCount() {
-    const previousCookies = parseInt(cookiesCount.textContent) || 0; // Obtém o valor anterior
+    const previousCookies = parseInt(cookiesCount.textContent) || 0;
     cookiesCount.textContent = cookies;
 
     if (cookies > previousCookies) {
-        // Se a quantidade de cookies aumentou, aplique a animação
         cookiesCount.classList.remove('cookie-change');
         cookie.classList.add('cookie-change');
         setTimeout(() => {
             cookie.classList.remove('cookie-change');
-        }, 100); // Tempo da animação em milissegundos
+        }, 100);
     }
 
     updateButtons();
 }
 
-setInterval(autoClick, 1000); // Chama a função autoClick a cada 1 segundo
+setInterval(autoClick, 1000);
 updateButtons();
 
-// Adicionando a função de clique no cookie
 cookie.addEventListener('click', () => {
-    cookies++;
+    if (bonusActive) {
+        cookies += clickMultiplier; // Aplica o bônus
+    } else {
+        cookies++; // Sem bônus
+    }
     updateCookiesCount();
     updateButtons();
-});
+    clickCount++; // Incrementa a contagem de cliques
 
-// Tenta tira o zoom da pagina
+    if (clickCount >= clickGoal && !bonusActive) { // Ative o bônus após 10 cliques, se não estiver ativo
+        applyClickMultiplier();
+        bonusActive = true; // Bônus ativado
+    }
+
+    // Cancela e redefine o temporizador do bônus em cada clique
+    clearTimeout(bonusTimeout);
+
+    // Adicione um if aqui para desativar o bônus somente se o tempo for maior que 1 segundo
+    if (clickCount >= clickGoal && bonusActive) {
+        bonusTimeout = setTimeout(() => {
+            clickCount = 0;
+            clickMultiplier = 1;
+            bonusMessage.style.display = 'none'; // Oculta a mensagem de bônus
+            bonusActive = false; // Bônus desativado
+        }, timeForBonusOff); // segundos em milissegundos
+    }
+});
 document.addEventListener('gesturestart', function (e) {
     e.preventDefault();
 });
